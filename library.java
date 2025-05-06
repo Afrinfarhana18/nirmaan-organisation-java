ec1c8a590e86d0caed16460e76be58d8444a5b3e
Afrinfarhana18 <afrinfarhana185@gmail.com>
2025-05-06 11:38:17 +0530

    Add files via upload

---
+++ library.java
@@ -0,0 +1,61 @@
+package day5;
+
+ public class library {
+	
+	 int id;
+	 String bookname;
+	 int prize;
+	 String authorname;
+	 
+ public static void main(String[] args) {
+	
+	  //libray1
+	  library l1 = new library();
+	  l1.id = 11111;
+      l1.bookname ="Mehala";
+      l1.prize = 340;
+      l1.authorname = "Pappu";
+    		  
+      //library2
+      library l2 = new library();
+      l2.id =22222;
+      l2.bookname = "life success";
+      l2.prize = 250;
+      l2.authorname = "albert";
+      
+      //library3
+      library l3 = new library();
+      l3.id =33333;
+      l3.bookname = "History";
+      l3.prize = 450;
+      l3.authorname = "sandhiya";
+      
+      //library4
+      library l4 = new library();
+      l4.id =44444;
+      l4.bookname = "Mehala story";
+      l4.prize = 550;
+      l4.authorname = "selvi";
+      
+      //library5
+      library l5 = new library();
+      l5.id =55555;
+      l5.bookname = "Renuka story";
+      l5.prize = 650;
+      l5.authorname = "Venky";
+      
+      System.out.println(l1.bookname);
+      System.out.println(l2.bookname);
+      System.out.println(l3.bookname);
+      System.out.println(l4.bookname);
+      System.out.println(l5.bookname);
+      
+    		  
+    		  
+      
+     
+    		  
+    		  
+}
+
+}
