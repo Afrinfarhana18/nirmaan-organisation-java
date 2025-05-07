5dfa4c61036e74419e4ed8365319355025a130a2
Afrinfarhana18 <afrinfarhana185@gmail.com>
2025-05-07 20:33:12 +0530

    Add files via upload

---
+++ reversenumber.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class reversenumber {
+	  public static void main(String[] args) {
+	
+		  for(int i=50;i>=1;i--)
+		  {
+			 System.out.println(i);
+		  }
+    }
+}         
+	         
+	       
