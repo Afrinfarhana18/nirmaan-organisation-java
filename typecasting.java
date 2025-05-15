cf3a097e330e6975a6f6d6347efa1018ac2aef9c
Afrinfarhana18 <afrinfarhana185@gmail.com>
2025-05-15 20:15:43 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,18 @@
+package day7;
+
+public class typecasting {
+	//implicity 
+		//int num = 10;
+		//double result = num; {// int to double
+		//System.out.println(result); // Output: 10.0
+		
+		//explicity 
+		double value = 7.7;
+		int result = (int) value; { // double to int
+		System.out.println(result); // Output: 9
+
+		}
+		
+	}
+
+}
