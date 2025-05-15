f6ff7b17ac0205438d8722a0f3c541cfefd55501
Afrinfarhana18 <afrinfarhana185@gmail.com>
2025-05-15 19:58:44 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,45 @@
+package day14;
+
+public class student {
+	
+	private int id;
+	private String name;
+	private int roll;
+	
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public int getRoll()
+	{
+		return roll;
+	}
+	public void setRoll(int roll) 
+	{
+		this.roll = roll;
+	} 
+	student(int id, String name, int rollno)
+	{
+		this.id=id;
+		
+		public String toString() 
+		{
+			return "student [id=" + id + ", name=" + name + ", rollno=" + rollno + "]";
+			
+}
+	}
+	
+
+}
